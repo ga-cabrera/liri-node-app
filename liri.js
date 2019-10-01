41ef403f4511a79410f285b9a6f56e9b01e6f459
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-09-30 18:43:17 -0700

    added axios to liri.js

--- liri.js
+++ liri.js
@@ -3,3 +3,8 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new spotify(keys.spotify);
 var fs = require("fs");
+
+axios.all([
+    axios.get("http://www.omdbapi.com"),
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+])

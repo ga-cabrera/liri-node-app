98a0766ed370c2c6afe1ddfd8d0a4cf203c05b8c
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-10-02 19:55:38 -0700

    added switch case for our functions

--- liri.js
+++ liri.js
@@ -1,15 +1,29 @@
-//read and set enviornment variables
-require("dotenv").config();
-
-//VARIABLES
-var axios = require("axios");
-var keys = require("./keys.js");
-var spotifySearch = require("node-spotify-api");
-var moment = require("moment");
+//Libraries
+require('dotenv').config();
+var keys = require('./keys.js');
 var spotify = new spotify(keys.spotify);
-var fs = require("fs");
-// variables that capture user input
-var liriFunction = process.argv[2];
+var Spotify = require('node-spotify-api');
+var fs = require('fs');
+var moment = require('moment');
+//Variables that capture user input.
+var command = process.argv[2];
 var userInput = process.argv[3];
 
+//Switch/case for your functions
+switch (command) {
+    case 'concert-this':
+        concertThis(userInput);
+        break;
+    case 'spotify-this-song':
+        spotifyThisSong(userInput);
+        break;
+    case 'movie-this':
+        movieThis(userInput);
+        break;
+    case 'do-what-it-says':
+        doSomething();
+        break;
+    default:
+        console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says");
+};
 

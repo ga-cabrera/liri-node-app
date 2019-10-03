3b04fb408441a0f52270b7bfda1f9f8fd6d6900b
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-10-02 19:12:22 -0700

    added all global variables

--- liri.js
+++ liri.js
@@ -1,10 +1,15 @@
+//read and set enviornment variables
 require("dotenv").config();
 
+//VARIABLES
+var axios = require("axios");
 var keys = require("./keys.js");
+var spotifySearch = require("node-spotify-api");
+var moment = require("moment");
 var spotify = new spotify(keys.spotify);
 var fs = require("fs");
+// variables that capture user input
+var liriFunction = process.argv[2];
+var userInput = process.argv[3];
 
-axios.all([
-    axios.get("http://www.omdbapi.com"),
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-])
+
